8294c0c08d713059bc20a63ee2d55af1a92df19e
rwerner1 <richard.werner@stud.htwk-leipzig.de>
2022-07-22 03:21:53 +0200

    autorx template added

--- src/Components/Views/Autor/AutorX.js
+++ src/Components/Views/Autor/AutorX.js
@@ -1,10 +1,38 @@
 import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function AutorX (){
+function AutorX() {
+    const [author, setAuthor] = useState();
+    const { id } = useParams();
+    
+    
+    const url = `http://localhost:8080/users/${id}`;
+
+    let content = <div>Author not found</div>;
+
+    useEffect(() => {
+        axios.get(url)
+            .then(response => {
+                console.log(response);
+                setAuthor(response.data);
+            });
+    }, [url]);
+
+    if (author) {
+        content = <div>
+            <p>{author.author}</p>
+            <p>{author.vorname}</p>
+            <p>{author.nachname}</p>
+            <p>
+                <img src={author.profilBild}></img>
+            </p>
+        </div>;
+    }
+
     return (
-        <div>
-            <p>Hier kommen die Daten des Autors hin. Bild, name, vorname, username</p>
-        </div>
+        <div>{content}</div>
     )
 }
 
