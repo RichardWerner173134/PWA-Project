f6bccb871d1e87e9c7e8edb8c72d607483882465
rwerner1 <richard.werner@stud.htwk-leipzig.de>
2022-09-18 17:29:37 +0200

    +save

--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -92,7 +92,7 @@ function NavigationMenu(props) {
                             className='text-blue-500 py-3 border-t border.b block'
                             onClick={props.closeMenu}
                         >
-                            Home
+                            Startseite
                         </Link>
                     </li>
                     <li>
@@ -101,7 +101,7 @@ function NavigationMenu(props) {
                             className='text-blue-500 py-3 border-t border.b block'
                             onClick={props.closeMenu}
                         >
-                            About
+                            Impressum
                         </Link>
                     </li>
                     <li>
